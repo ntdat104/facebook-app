cced91562f92a7800108796db149397cce7b8081
minhhung <minhhunghuynh1106@gmail.com>
2021-09-04 22:35:51 +0700

    done 90% register form logic 🍯

--- client/src/pages/register/RegisterForm.tsx
+++ client/src/pages/register/RegisterForm.tsx
@@ -1,21 +1,63 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 // clsx
 import clsx from 'clsx';
 
 // material ui core
 import { Avatar } from '@material-ui/core';
 
+import { registerUserForm } from '@/apis/authApi';
+import { randomAvatar } from '@/apis/avatarApi';
+
 import styles from '@/styles/utilities.module.scss';
 
+interface IFormState {
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
+
 function RegisterForm() {
+  const [avatar, setAvatar] = useState<string>('');
+  const [formData, setFormData] = useState<IFormState>({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    registerUserForm({
+      username: formData.username,
+      password: formData.password,
+      avatar,
+    });
+  };
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const getRandomAvatar = () => {
+    setAvatar(randomAvatar());
+  };
+
   return (
-    <form className={clsx('mb-3 w-full')}>
+    <form onSubmit={handleSubmitForm} className={clsx('mb-3 w-full')}>
       <div>
         <input
           className={clsx(
             'w-full rounded-lg px-4 py-3 text-base outline-none border-2 border-gray-dark',
             'text-light-text'
           )}
-          placeholder='Email address or phone number'
+          placeholder='Username'
+          name='username'
+          value={formData.username}
+          onChange={handleChangeInput}
         />
       </div>
       <div className={clsx('my-4')}>
@@ -25,6 +67,9 @@ function RegisterForm() {
             'text-light-text'
           )}
           placeholder='Password'
+          name='password'
+          value={formData.password}
+          onChange={handleChangeInput}
         />
       </div>
       <div className={clsx('my-4')}>
@@ -33,18 +78,21 @@ function RegisterForm() {
             'w-full rounded-lg px-4 py-3 text-base outline-none border-2 border-gray-dark',
             'text-light-text'
           )}
+          name='confirmPassword'
           placeholder='Confirm password'
+          value={formData.confirmPassword}
+          onChange={handleChangeInput}
         />
       </div>
-      <div className={clsx('i-flex-center mb-4')}>
+      <div className={clsx('i-flex-center mb-4')} onClick={getRandomAvatar}>
         <Avatar
           className={clsx('cursor-pointer', styles.circleSizeLarge)}
-          src='https://avatars.dicebear.com/api/micah/123.svg'
+          src={avatar}
         />
       </div>
       <button
         className={clsx(
-          'h-12 rounded-lg text-xl font-bold w-full',
+          'h-12 rounded-lg text-xl font-bold w-full outline-none',
           'text-white bg-primary',
           'transition ease-out',
           'hover:brightness-95'
---
+++ client/src/utils/avatarList.ts
@@ -0,0 +1,12 @@
+export const avatarList = [
+  'avataaars',
+  'croodles',
+  'croodles-neutral',
+  'human',
+  'male',
+  'female',
+  'micah',
+  'open-peeps',
+  'pixel-art-neutral',
+  'pixel-art',
+];
